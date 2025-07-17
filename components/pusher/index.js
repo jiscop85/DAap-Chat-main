1fe85a5ec9fc6bca7f868ca7f1edd47ccd8682e8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-07-18 01:16:30 +0330

    Add files via upload

---
+++ components/pusher/index.js
@@ -0,0 +1,19 @@
+import Pusher from "pusher";
+
+export const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "eu",
+  useTLS: true,
+});
+
+export default async function handler(req, res) {
+  const { message, user } = req.body;
+  const response = await pusher.trigger("chat", "chat-event", {
+    message,
+    user,
+  });
+
+  res.json({ message: "completed" });
+}
